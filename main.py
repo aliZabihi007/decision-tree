6be650627d63f964e6d5215dbd625b677f7a48d3
Ali <zabihiali1998@gmail.com>
2020-11-30 00:56:57 +0330

    part(1)

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+
+
+
+def print_hi(name):
+   plt.rcParams['figure.figsize']=(10,6)
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
